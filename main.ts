494038ade18bfdcbd724285d91afb1fba3e67c88
frans76 <61806471+frans76@users.noreply.github.com>
2022-08-27 14:56:46 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,52 @@
+input.onButtonPressed(Button.A, function () {
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+    basic.showString("poep")
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("papa en mama en aster zijn lief")
+    basic.showIcon(IconNames.Ghost)
+    basic.showIcon(IconNames.Skull)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # .
+        . # # # #
+        `)
+    music.playMelody("B A - - G - - C ", 12 ** 1)
+    wuKong.setAllMotor(100, 100)
+})
+input.onButtonPressed(Button.B, function () {
+    music.setVolume(127)
+    music.playTone(988, music.beat(BeatFraction.Breve))
+    music.playTone(131, music.beat(BeatFraction.Breve))
+    if (0 == music.beat(BeatFraction.Breve)) {
+        music.playTone(349, music.beat(BeatFraction.Breve))
+    }
+})
 basic.forever(function () {
 	
 })
